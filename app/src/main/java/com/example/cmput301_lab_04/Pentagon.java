3615fad6a7208d8a1e6edd9b0eec35b95ed5c24d
Zeemo10 <zeemeau@gmail.com>
2026-01-29 18:13:46 -0700

    deleted circle made pentagon class

---
+++ app/src/main/java/com/example/cmput301_lab_04/Pentagon.java
@@ -0,0 +1,7 @@
+package com.example.cmput301_lab_04;
+
+public class Pentagon extends Shape {
+    int x, y;
+    double side;
+    double area;
+}
--- app/src/main/java/com/example/cmput301_lab_04/circle.java
+++
@@ -1,6 +0,0 @@
-package com.example.cmput301_lab_04;
-
-public class circle extends Shape{
-    int x, y;
-    double radius;
-}
